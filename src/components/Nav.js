1d790c84eab8261898870ec69c095765039802b3
Arvind Maan <arvindmaan1199@gmail.com>
2020-05-03 12:42:14 -0500

    set up core components

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+    <nav>
+        <div className="nav-container">
+            <a href="#" classname="logo">Find-Me</a> 
+        </div>
+    </nav>
+    );
+}
+
+export default Nav;
